782704804273586878cd354f9cf7d38621581610
lizzz0523 <lizzzled@gmail.com>
2013-11-28 02:21:38 +0800

    change tab to 4 space

--- zita.js
+++ zita.js
@@ -1,5 +1,5 @@
 /*
-	by liz
+    by liz
 */
 
 ;(function(exports, undefined){
@@ -10,163 +10,163 @@ var zita = exports.zita = {};
 // array or object
 
 var Slice = function(arr, start, end){
-	return Array.prototype.slice.call(arr, start, end);
+    return Array.prototype.slice.call(arr, start, end);
 }
 
 zita.each = function(obj, callback){
-	var i = 0, key;
+    var i = 0, key;
 
-	if(obj == null) return;
+    if(obj == null) return;
 
-	if(obj.forEach){
-		obj.forEach(function(){
-			return callback.apply(zita, arguments);
-		})
+    if(obj.forEach){
+        obj.forEach(function(){
+            return callback.apply(zita, arguments);
+        })
 
-		return;
-	}
+        return;
+    }
 
-	if(obj.length){
-		// if obj is array
-		for(; i < obj.length; i++){
-			if(callback.call(zita, i, obj[i], obj) === false) break;
-		}
-	}else{
-		// if obj is object
-		key = zita.key(obj);
-		for(; i < key.length; i++){
-			if(callback.call(zita, key[i], obj[key[i]], obj) === false) break;
-		}
-	}
+    if(obj.length){
+        // if obj is array
+        for(; i < obj.length; i++){
+            if(callback.call(zita, i, obj[i], obj) === false) break;
+        }
+    }else{
+        // if obj is object
+        key = zita.key(obj);
+        for(; i < key.length; i++){
+            if(callback.call(zita, key[i], obj[key[i]], obj) === false) break;
+        }
+    }
 
-	return;
+    return;
 }
 
 zita.merge = function(dest, orig){
-	var res = Slice(dest, 1);
+    var res = Slice(dest, 1);
 
-	zita.each(orig, function(value){
-		res.push(value);
-	});
+    zita.each(orig, function(value){
+        res.push(value);
+    });
 
-	return res;
+    return res;
 }
 
 var IS_DONTENUM_BUG = (function(){
-	for(var prop in {toString : 1}){
-		if(prop == 'toString') return false;
-	}
-	return true;
+    for(var prop in {toString : 1}){
+        if(prop == 'toString') return false;
+    }
+    return true;
 })();
 
 zita.keys = function(obj){
-	var res = [],
-		prop;
-	
-	for(prop in obj){
-		if(!obj.hasOwnProperty(prop)) continue;
-		res.push(prop);
-	}
+    var res = [],
+        prop;
+    
+    for(prop in obj){
+        if(!obj.hasOwnProperty(prop)) continue;
+        res.push(prop);
+    }
 
-	// fix don't enum bug
-	if(IS_DONTENUM_BUG){
-		if(obj.toString !== Object.prototype.toString){
-			res.push('toString');
-		}
+    // fix don't enum bug
+    if(IS_DONTENUM_BUG){
+        if(obj.toString !== Object.prototype.toString){
+            res.push('toString');
+        }
 
-		if(obj.valueOf !== Object.prototype.valueOf){
-			res.push('valueOf');
-		}
-	}
+        if(obj.valueOf !== Object.prototype.valueOf){
+            res.push('valueOf');
+        }
+    }
 
-	return res;
+    return res;
 }
 
 zita.values = function(obj){
-	var res = [];
-		prop;
+    var res = [];
+        prop;
 
-	for(prop in obj){
-		if(!obj.hasOwnProperty(prop)) continue;
-		res.push(obj[prop]);
-	}
+    for(prop in obj){
+        if(!obj.hasOwnProperty(prop)) continue;
+        res.push(obj[prop]);
+    }
 
-	return res;
+    return res;
 }
 
 zita.extend = function(dest, orig){
-	var keys = zita.keys(orig);
+    var keys = zita.keys(orig);
 
-	zita.each(keys, function(key){
-		dest[key] = orig[key];
-	});
+    zita.each(keys, function(key){
+        dest[key] = orig[key];
+    });
 
-	return dest;
+    return dest;
 }
 
 
 // function
 
 zita.debounce = function(callback, delay){
-	var timer = null;
+    var timer = null;
 
-	function clear(){
-		clearTimeout(timer);
-		timer = null;
-	}
+    function clear(){
+        clearTimeout(timer);
+        timer = null;
+    }
 
-	delay = delay || 200;
+    delay = delay || 200;
 
-	return function(){
-		var orig = callback,
-			context = this,
-			args = arguments;
+    return function(){
+        var orig = callback,
+            context = this,
+            args = arguments;
 
-		callback = function(){
-			clear();
-			orig.apply(context, args);
-		}
+        callback = function(){
+            clear();
+            orig.apply(context, args);
+        }
 
-		timer && clear();
-		timer = setTimeout(callback, delay);
-	}
+        timer && clear();
+        timer = setTimeout(callback, delay);
+    }
 }
 
 zita.pulse = function(callback, period, delay){
-	var timer = null,
-		endtime, active;
+    var timer = null,
+        endtime, active;
 
-	function now(){
-		return (new Date).getTime();
-	}
+    function now(){
+        return (new Date).getTime();
+    }
 
-	function clear(){
-		clearInterval(timer);
-		timer = null;
-		active = false;
-	}
+    function clear(){
+        clearInterval(timer);
+        timer = null;
+        active = false;
+    }
 
-	delay = delay || 200;
-	period = period || 1000;
+    delay = delay || 200;
+    period = period || 1000;
 
-	return function(){
-		var orig = callback,
-			context = this,
-			args = arguments;
+    return function(){
+        var orig = callback,
+            context = this,
+            args = arguments;
 
-		callback = function(){
-			orig.apply(context, args);
-			if(now() > endtime){
-				clear();
-			}
-		}
+        callback = function(){
+            orig.apply(context, args);
+            if(now() > endtime){
+                clear();
+            }
+        }
 
-		endtime = now() + period;
-		if(!active){
-			active = true;
-			timer = setInterval(callback, delay);
-		}
-	}
+        endtime = now() + period;
+        if(!active){
+            active = true;
+            timer = setInterval(callback, delay);
+        }
+    }
 }
 
 
@@ -174,124 +174,124 @@ zita.pulse = function(callback, period, delay){
 
 zita.Node = (function(){
 
-	var win = window,
-		doc = document,
-		docRoot = doc.documentElement,
-		body = doc.body;
+    var win = window,
+        doc = document,
+        docRoot = doc.documentElement,
+        body = doc.body;
 
-	function getElementNode(node){
-		if(node && node.nodeType){
-			if(node.nodeType == 3){
-				node = node.parentNode;
-			}
+    function getElementNode(node){
+        if(node && node.nodeType){
+            if(node.nodeType == 3){
+                node = node.parentNode;
+            }
 
-			return node;
-		}
+            return node;
+        }
 
-		if(node && node.zita){
-			return node.self;
-		}
+        if(node && node.zita){
+            return node.self;
+        }
 
-		return false
-	}
+        return false
+    }
 
-	function Node(node){
-		if(node.length){
-			node = node[0];
-		}
+    function Node(node){
+        if(node.length){
+            node = node[0];
+        }
 
-		if(!(node = getElementNode(node))){
-			throw Error('not element node');
-		}
+        if(!(node = getElementNode(node))){
+            throw Error('not element node');
+        }
 
-		this.self = node;
-	}
+        this.self = node;
+    }
 
-	Node.fn = Node.prototype = {
-		zita : '0.0.1'
-	}
+    Node.fn = Node.prototype = {
+        zita : '0.0.1'
+    }
 
-	Node.fn.contains = function(node){
-		var self = this.self,
-			cur;
+    Node.fn.contains = function(node){
+        var self = this.self,
+            cur;
 
-		if(!(node = getElementNode(node))) return false;
+        if(!(node = getElementNode(node))) return false;
 
-		if(self.contains){
-			return self.contains(node);
-		}
+        if(self.contains){
+            return self.contains(node);
+        }
 
-		if(self.compareDocumentPosition){
-			// DOCUMENT_POSITION_DISCONNECTED            1
-			// DOCUMENT_POSITION_PRECEDING               2
-			// DOCUMENT_POSITION_FOLLOWING               4
-			// DOCUMENT_POSITION_CONTAINS                8
-			// DOCUMENT_POSITION_CONTAINED_BY            16
-			// DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC 32
-			return !!(self.compareDocumentPosition(node) & 16);
-		}
+        if(self.compareDocumentPosition){
+            // DOCUMENT_POSITION_DISCONNECTED            1
+            // DOCUMENT_POSITION_PRECEDING               2
+            // DOCUMENT_POSITION_FOLLOWING               4
+            // DOCUMENT_POSITION_CONTAINS                8
+            // DOCUMENT_POSITION_CONTAINED_BY            16
+            // DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC 32
+            return !!(self.compareDocumentPosition(node) & 16);
+        }
 
-		cur = node;
-		while(cur){
-			if(cur == self) return true;
-			cur = node.parentNode;
-		}
+        cur = node;
+        while(cur){
+            if(cur == self) return true;
+            cur = node.parentNode;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	Node.fn.offset = function(){
-		var self = this.self,
-			cur, offsetParent,
-			crect,
-			rect = null;
+    Node.fn.offset = function(){
+        var self = this.self,
+            cur, offsetParent,
+            crect,
+            rect = null;
 
-		if(self.getBoundingClientRect){
-			crect = self.getBoundingClientRect();
-			rect = {};
+        if(self.getBoundingClientRect){
+            crect = self.getBoundingClientRect();
+            rect = {};
 
-			zita.each('left top'.split(' '), function(dir){
-				// firefox not round the top and bottom
-				if(crect[dir] === undefined){
-					rect = null;
-					return false;
-				}
-				rect[dir] = crect[dir];
-			});
-		}
+            zita.each('left top'.split(' '), function(dir){
+                // firefox not round the top and bottom
+                if(crect[dir] === undefined){
+                    rect = null;
+                    return false;
+                }
+                rect[dir] = crect[dir];
+            });
+        }
 
-		if(!rect){
-			offsetParent = self.offsetParent;
-			rect = {
-				left : self.offsetLeft,
-				top : self.offsetTop
-			};
+        if(!rect){
+            offsetParent = self.offsetParent;
+            rect = {
+                left : self.offsetLeft,
+                top : self.offsetTop
+            };
 
-			cur = self;
-			while((cur = cur.parentNode) && cur != body){
-				rect.left -= cur.scrollLeft;
-				rect.top += cur.scrollTop;
+            cur = self;
+            while((cur = cur.parentNode) && cur != body){
+                rect.left -= cur.scrollLeft;
+                rect.top += cur.scrollTop;
 
-				if(cur == offsetParent){
-					// use clientLeft/Top to replace calculating elem border width
-					rect.left += cur.offsetLeft + cur.clientLeft;
-					rect.top += cur.offsetTop + cur.clientTop;
+                if(cur == offsetParent){
+                    // use clientLeft/Top to replace calculating elem border width
+                    rect.left += cur.offsetLeft + cur.clientLeft;
+                    rect.top += cur.offsetTop + cur.clientTop;
 
-					offsetParent = cur.offsetParent;
-				}
-			}
-		}else{
-			// subtract clientLeft/Top to correct double counting
-			rect.left += (win.pageXOffset || docRoot.scrollLeft) - docRoot.clientLeft;
-			rect.top += (win.pageYOffset || docRoot.scrollTop) - docRoot.clientTop;
-		}
+                    offsetParent = cur.offsetParent;
+                }
+            }
+        }else{
+            // subtract clientLeft/Top to correct double counting
+            rect.left += (win.pageXOffset || docRoot.scrollLeft) - docRoot.clientLeft;
+            rect.top += (win.pageYOffset || docRoot.scrollTop) - docRoot.clientTop;
+        }
 
-		return rect;
-	}
+        return rect;
+    }
 
-	return function(node){
-		return new Node(node);
-	};
+    return function(node){
+        return new Node(node);
+    };
 
 })();
 
@@ -300,107 +300,107 @@ zita.Node = (function(){
 
 zita.Ticker = (function(){
 
-	var win = exports,
+    var win = exports,
 
-		// internal identity is used to mark a registered function
-		tickId = 0,
-		tickers = [],
+        // internal identity is used to mark a registered function
+        tickId = 0,
+        tickers = [],
 
-		timerId = null,
+        timerId = null,
 
-		requestAnimFrame,
-		cancelAnimFrame,
+        requestAnimFrame,
+        cancelAnimFrame,
 
-		settings = {
-			useRAF : true,
-			interval : 1000 / 60
-		};
-	
-	(function(){
-		var vendors = ' ms moz webkit o'.split(' '),
-			i = 0,
-			len = vendors.length;
+        settings = {
+            useRAF : true,
+            interval : 1000 / 60
+        };
+    
+    (function(){
+        var vendors = ' ms moz webkit o'.split(' '),
+            i = 0,
+            len = vendors.length;
 
-		if(settings.useRAF){
-			for(; i < len && !requestAnimFrame; i++){
-				requestAnimFrame = win[vendors[i] + 'RequestAnimationFrame'];
-				cancelAnimFrame = win[vendors[i] + 'CancelAnimationFrame'] || win[vendors[i] + 'CancelRequestAnimationFrame'];
-			}
-		}
+        if(settings.useRAF){
+            for(; i < len && !requestAnimFrame; i++){
+                requestAnimFrame = win[vendors[i] + 'RequestAnimationFrame'];
+                cancelAnimFrame = win[vendors[i] + 'CancelAnimationFrame'] || win[vendors[i] + 'CancelRequestAnimationFrame'];
+            }
+        }
 
-		requestAnimFrame || (requestAnimFrame = function(callback){
-			return win.setTimeout(callback, settings.interval);
-		});
+        requestAnimFrame || (requestAnimFrame = function(callback){
+            return win.setTimeout(callback, settings.interval);
+        });
 
-		cancelAnimFrame || (cancelAnimFrame = function(timerId){
-			return win.clearTimeout(timerId);
-		});
-	})();
-	
-	function tick(){
-		var ticker,
-			i = 0,
-			len = tickers.length;
-			
-		for(; i < len; i++){
-			ticker = tickers[i];
-			ticker.callback.call(ticker.context);
-		}
-	}
+        cancelAnimFrame || (cancelAnimFrame = function(timerId){
+            return win.clearTimeout(timerId);
+        });
+    })();
+    
+    function tick(){
+        var ticker,
+            i = 0,
+            len = tickers.length;
+            
+        for(; i < len; i++){
+            ticker = tickers[i];
+            ticker.callback.call(ticker.context);
+        }
+    }
 
-	function run(){
-		timerId = requestAnimFrame(arguments.callee);
-		tick();
-	}
-	
-	function stop(){
-		cancelAnimFrame(timerId);
-		timerId = null;
-	}
+    function run(){
+        timerId = requestAnimFrame(arguments.callee);
+        tick();
+    }
+    
+    function stop(){
+        cancelAnimFrame(timerId);
+        timerId = null;
+    }
 
-	return {
-		add : function(callback, context){
-			// if the callback function has registered, skip it
-			if(callback.tickId) return;
+    return {
+        add : function(callback, context){
+            // if the callback function has registered, skip it
+            if(callback.tickId) return;
 
-			callback.tickId = 'tick-' + tickId++;
-			tickers.push({
-				callback : callback,
-				context : context || win
-			});
+            callback.tickId = 'tick-' + tickId++;
+            tickers.push({
+                callback : callback,
+                context : context || win
+            });
 
-			if(tickers.length == 1) run();
+            if(tickers.length == 1) run();
 
-			return callback;
-		},
-		remove : function(callback){
-			var ticker,
-				i = 0,
-				len = runers.length;
+            return callback;
+        },
+        remove : function(callback){
+            var ticker,
+                i = 0,
+                len = runers.length;
 
-			if(!callback){
-				tickers = [];
-			}else{
-				if(!callback.tickId) return;
+            if(!callback){
+                tickers = [];
+            }else{
+                if(!callback.tickId) return;
 
-				for(; i < len; i++){
-					ticker = tickers[i];
-					if(callback.tickId == ticker.callback.tickId){
-						callback = tickers.splice(i, 1);
-						delete callback.tickId;
-						break;
-					}
-				}
-			}
-			
-			if(!tickers.length){
-				stop();
-			}
+                for(; i < len; i++){
+                    ticker = tickers[i];
+                    if(callback.tickId == ticker.callback.tickId){
+                        callback = tickers.splice(i, 1);
+                        delete callback.tickId;
+                        break;
+                    }
+                }
+            }
+            
+            if(!tickers.length){
+                stop();
+            }
 
-			return callback;
-		}
-	}
-		
+            return callback;
+        }
+    }
+
 })();
 
 
