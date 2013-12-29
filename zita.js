9d7ef0753ae7019df850d0a7ca40e39e0dae98aa
lizzz0523 <lizzzled@gmail.com>
2013-12-30 04:40:03 +0800

    add some useful function

--- zita.js
+++ zita.js
@@ -30,263 +30,6 @@ zita.extend = function(dest){
 };
 
 
-// array or object
-
-var IS_DONTENUM_BUG = (function(){
-    for(var prop in {toString : 1}){
-        if(prop == 'toString') return false;
-    }
-    return true;
-})();
-
-var _each = zita.each = function(list, iterator){
-    var keys,
-        i = 0;
-
-    if(list == null) return;
-
-    // use the native ecmascript 5 each;
-    if(list.forEach){
-        list.forEach(function(){
-            return iterator.apply(zita, arguments);
-        });
-
-        return;
-    }
-
-    if(list.length){
-        // if list is array
-        for(; i < list.length; i++){
-            if(iterator.call(zita, list[i], i, list) === false) break;
-        }
-    }else{
-        // if list is object
-        keys = zita.keys(list);
-        for(; i < keys.length; i++){
-            if(iterator.call(zita, list[keys[i]], keys[i], list) === false) break;
-        }
-    }
-
-    return;
-};
-
-zita.map = function(list, iterator){
-    var res = list.length ? [] : {};
-
-    if(list.map && list.map === Array.prototype.map){
-        return list.map(iterator, zita);
-    }
-
-    _each(list, function(value, key){
-        res[key] = iterator.apply(zita, arguments);
-    });
-
-    return res;
-}
-
-zita.reduce = function(list, iterator, memo){
-    var res = memo;
-
-    if(list.reduce && list.reduce === Array.prototype.reduce){
-        iterator = zita.bind(iterator, zita);
-        return res ? list.reduce(iterator, res) : list.reduce(iterator);
-    }
-
-    _each(list, function(){
-        if(res == undefined){
-            res = arguments[0];
-        }else{
-            res = iterator.apply(zita, zita.merge([res], _slice(arguments)));
-        }
-    });
-
-    return res;
-};
-
-zita.max = function(list, iterator){
-    var proxy, max,
-        i = 0;
-
-    max = {proxy : -Infinity, value : -Infinity};
-    _each(list, function(value){
-        proxy = iterator ? iterator.apply(zita, arguments) : value;
-        if(proxy > max.proxy){
-            max.proxy = proxy;
-            max.value = value;
-        }
-    });
-
-    return max.value;
-};
-
-zita.min = function(list, iterator){
-    var proxy, min,
-        i = 0;
-
-    min = {proxy : Infinity, value : Infinity};
-    _each(list, function(value){
-        proxy = iterator ? iterator.apply(zita, arguments) : value;
-        if(proxy < min.proxy){
-            min.proxy = proxy;
-            min.value = value;
-        }
-    });
-
-    return min.value;
-};
-
-zita.merge = function(dest){
-    var args = _slice(arguments, 1),
-        src, keys,
-        i = 0,
-        j = 0;
-
-    for(; i < args.length; i++){
-        src = args[i];
-
-        if(dest.length){
-            dest = dest.concat(src);
-        }else{
-            keys = zita.keys(src);
-            for(; j < keys.length; j++){
-                dest[keys[j]] = obj[keys[j]];
-            }
-        }
-    }
-
-    return dest;
-};
-
-zita.clone = function(orig){
-    return zita.merge(orig.length ? [] : {}, orig);
-};
-
-zita.range = function(start, stop, step){
-    var res = [],
-        i, len;
-
-    if(arguments.length <= 1){
-        stop = start;
-        start = 0;
-    }
-    step = step || 1;
-
-    i = start;
-    len = Math.max(Math.ceil((stop - start) / step, 0));
-    while(len--){
-        res.push(start);
-        start += step;
-    };
-
-    return res;
-};
-
-zita.keys = function(obj){
-    var res = [],
-        prop;
-    
-    for(prop in obj){
-        if(!obj.hasOwnProperty(prop)) continue;
-        res.push(prop);
-    }
-
-    // fix don't enum bug
-    if(IS_DONTENUM_BUG){
-        if(obj.toString !== Object.prototype.toString){
-            res.push('toString');
-        }
-
-        if(obj.valueOf !== Object.prototype.valueOf){
-            res.push('valueOf');
-        }
-    }
-
-    return res;
-};
-
-zita.values = function(obj){
-    var res = [];
-        prop;
-
-    for(prop in obj){
-        if(!obj.hasOwnProperty(prop)) continue;
-        res.push(obj[prop]);
-    }
-
-    // fix don't enum bug
-    if(IS_DONTENUM_BUG){
-        if(obj.toString !== Object.prototype.toString){
-            res.push(obj.toString);
-        }
-
-        if(obj.valueOf !== Object.prototype.valueOf){
-            res.push(obj.valueOf);
-        }
-    }
-
-    return res;
-};
-
-var _type = zita.type = (function(){
-
-    var class2type = {};
-
-    _each('Number Boolean String Array Object Function Date RegExp Error'.split(' '), function(type){
-        class2type['[object ' + type + ']'] = type.toLowerCase();
-    });
-
-    return function(obj){
-        if(obj == null){
-            return String(obj);
-        }
-
-        return typeof obj == 'object' || typeof obj == 'function'
-            ? class2type[_string(obj)] || "object"
-            : typeof obj;
-    }
-
-})();
-
-_each('Number Boolean String Date RegExp'.split(' '), function(type){
-    zita['is' + type] = function(obj){ return _type(obj) == type.toLowerCase(); };
-});
-
-zita.isUndefined = function(obj){
-    return obj == undefined;
-};
-
-zita.isNull = function(obj){
-    return obj == null;
-};
-
-zita.isArray = Array.isArray || function(obj){
-    return _type(obj) == 'array';
-};
-
-// fix chrome(1-12) bug and use a faster solution
-zita.isFunction = typeof /./ != 'function'
-? function(obj){
-    return typeof obj == 'function';
-}
-: function(obj){
-    return _type(obj) == 'function';
-};
-
-zita.isElement = function(obj){
-    return obj != null && obj.nodeType && obj.nodeType == 1;
-};
-
-zita.isNaN = Number.isNaN || function(obj){
-    return _type(obj) == 'number' && obj !== obj ;
-};
-
-zita.isFinite = function(obj){
-    // !isNaN(parseFloat(obj)) for numeric check
-    // isFinite(obj) for finite check
-    return !isNaN(parseFloat(obj)) && isFinite(obj);
-};
-
-
 // function
 
 zita.bind = (function(){
@@ -389,6 +132,392 @@ zita.delay = function(callback, delay){
 zita.defer = function(callback){
     var args = zita.merge([callback, 0.01], _slice(arguments, 1));
     return zita.delay.apply(zita, args);
+};
+
+
+// array or object
+
+var IS_DONTENUM_BUG = (function(){
+    for(var prop in {toString : 1}){
+        if(prop == 'toString') return false;
+    }
+    return true;
+})();
+
+var _each = zita.each = function(list, iterator){
+    var keys,
+        i = 0;
+
+    if(list == null) return;
+
+    // use the native ecmascript 5 each;
+    if(list.forEach){
+        list.forEach(function(){
+            return iterator.apply(zita, arguments);
+        });
+
+        return;
+    }
+
+    if(list.length){
+        // if list is array
+        for(; i < list.length; i++){
+            if(iterator.call(zita, list[i], i, list) === false) break;
+        }
+    }else{
+        // if list is object
+        keys = zita.keys(list);
+        for(; i < keys.length; i++){
+            if(iterator.call(zita, list[keys[i]], keys[i], list) === false) break;
+        }
+    }
+
+    return;
+};
+
+zita.reduce = function(list, iterator, memo){
+    var res = memo;
+
+    if(list.reduce && list.reduce === Array.prototype.reduce){
+        iterator = zita.bind(iterator, zita);
+        return res == undefined ? list.reduce(iterator, res) : list.reduce(iterator);
+    }
+
+    _each(list, function(){
+        if(res == undefined){
+            res = arguments[0];
+        }else{
+            res = iterator.apply(zita, zita.merge([res], _slice(arguments)));
+        }
+    });
+
+    return res;
+};
+
+zita.map = function(list, iterator){
+    var res = list.length ? [] : {};
+
+    if(list.map && list.map === Array.prototype.map){
+        return list.map(iterator, zita);
+    }
+
+    _each(list, function(value, key){
+        res[key] = iterator.apply(zita, arguments);
+    });
+
+    return res;
+};
+
+zita.find = function(list, iterator){
+    var res;
+
+    if(list,find && list.find == Array.prototype.find){
+        return list.find(iterator, zita);
+    }
+
+    _each(list, function(value){
+        if(iterator.apply(zita, arguments)){
+            res = value;
+            return false;
+        }
+    });
+
+    return res;
+};
+
+zita.filter = function(list, iterator){
+    var res = list.length ? [] : {};
+
+    if(list.filter && list.filter === Array.prototype.filter){
+        return list.filter(iterator, zita);
+    }
+
+    _each(list, function(value, key){
+        if(iterator.apply(zita, arguments)){
+            res[key] = value;
+        }
+    });
+
+    return res;
+};
+
+zita.every = function(list, iterator){
+    var res = true;
+
+    if(list.every && list.every === Array.prototype.every){
+        return list.every(iterator, zita);
+    }
+
+    _each(list, function(){
+        if(!iterator.apply(zita, arguments)){
+            res = false;
+            return false;
+        }
+    });
+
+    return res;
+};
+
+zita.some = function(list, iterator){
+    var res = false;
+
+    if(list.some && list.som === Array.prototype.some){
+        return list.some(iterator, zita);
+    }
+
+    _each(list, function(){
+        if(iterator.apply(zita, arguments)){
+            res = true;
+            return false;
+        }
+    });
+
+    return res;
+};
+
+zita.contains = function(list, target){
+    return zita.some(list, function(value){
+        return value === target;
+    });
+};
+
+zita.without = function(list){
+    var args = _slice(arguments, 1);
+
+    return zita.filter(list, function(value){
+        return !zita.contains(args, value);
+    });
+};
+
+zita.max = function(list, iterator){
+    var proxy, max;
+
+    max = {proxy : -Infinity, value : -Infinity};
+    _each(list, function(value){
+        proxy = iterator ? iterator.apply(zita, arguments) : value;
+        if(proxy > max.proxy){
+            max.proxy = proxy;
+            max.value = value;
+        }
+    });
+
+    return max.value;
+};
+
+zita.min = function(list, iterator){
+    var proxy, min;
+
+    min = {proxy : Infinity, value : Infinity};
+    _each(list, function(value){
+        proxy = iterator ? iterator.apply(zita, arguments) : value;
+        if(proxy < min.proxy){
+            min.proxy = proxy;
+            min.value = value;
+        }
+    });
+
+    return min.value;
+};
+
+zita.merge = function(dest){
+    var args = _slice(arguments, 1),
+        src, keys,
+        i = 0,
+        j = 0;
+
+    for(; i < args.length; i++){
+        src = args[i];
+
+        if(dest.length){
+            dest = dest.concat(src);
+        }else{
+            keys = zita.keys(src);
+            for(; j < keys.length; j++){
+                dest[keys[j]] = src[keys[j]];
+            }
+        }
+    }
+
+    return dest;
+};
+
+zita.clone = function(orig){
+    return zita.merge(orig.length ? [] : {}, orig);
+};
+
+zita.size = function(list){
+    var keys;
+
+    if(list == null) return 0;
+
+    if(list.length){
+        return list.length;
+    }else{
+        keys = zita.keys(list);
+        return keys.length;
+    }
+};
+
+zita.range = function(start, stop, step){
+    var res = [],
+        i, len;
+
+    if(arguments.length <= 1){
+        stop = start;
+        start = 0;
+    }
+    step = step || 1;
+
+    i = start;
+    len = Math.max(Math.ceil((stop - start) / step, 0));
+    while(len--){
+        res.push(start);
+        start += step;
+    };
+
+    return res;
+};
+
+zita.first = function(arr, index){
+    var len = Math.min(index || 1, arr.length);
+    if(len < 0) return [];
+    return _slice(arr, 0, len);
+};
+
+zita.last = function(arr, index){
+    var len = Math.min(index || 1, arr.length);
+    if(len < 0) return _slice(arr);
+    return _slice(arr, arr.length - len);
+};
+
+zita.keys = function(obj){
+    var res = [],
+        prop;
+    
+    for(prop in obj){
+        if(!obj.hasOwnProperty(prop)) continue;
+        res.push(prop);
+    }
+
+    // fix don't enum bug
+    if(IS_DONTENUM_BUG){
+        if(obj.toString !== Object.prototype.toString){
+            res.push('toString');
+        }
+
+        if(obj.valueOf !== Object.prototype.valueOf){
+            res.push('valueOf');
+        }
+    }
+
+    return res;
+};
+
+zita.values = function(obj){
+    var res = [];
+        prop;
+
+    for(prop in obj){
+        if(!obj.hasOwnProperty(prop)) continue;
+        res.push(obj[prop]);
+    }
+
+    // fix don't enum bug
+    if(IS_DONTENUM_BUG){
+        if(obj.toString !== Object.prototype.toString){
+            res.push(obj.toString);
+        }
+
+        if(obj.valueOf !== Object.prototype.valueOf){
+            res.push(obj.valueOf);
+        }
+    }
+
+    return res;
+};
+
+zita.equal = function(a, b){
+    var type;
+
+    // for non-object condition: string/boolean/number
+    if(a === b) return a !== 0 || 1 / a === 1 / b;
+
+    if((type = _type(a)) != _type(b)) return false;
+    switch(type){
+        case 'string' :
+        case 'function' :
+            return String(a) == String(b);
+
+        case 'boolean' :
+        case 'number' :
+        case 'date' :
+            return +a == +b || (a !== a && b !== b) || (a == 0 && 1 / a == 1 / b);
+
+        case 'regexp' :
+            return a.source == b.source
+            && a.global == b.global
+            && a.multiline == b.multiline
+            && a.ignoreCase == b.ignoreCase;
+    }
+};
+
+var _type = zita.type = (function(){
+
+    var class2type = {};
+
+    _each('Number Boolean String Array Object Function Date RegExp Error'.split(' '), function(type){
+        class2type['[object ' + type + ']'] = type.toLowerCase();
+    });
+
+    return function(obj){
+        if(obj == null){
+            return String(obj);
+        }
+
+        return typeof obj == 'object' || typeof obj == 'function'
+            ? class2type[_string(obj)] || "object"
+            : typeof obj;
+    }
+
+})();
+
+_each('Number Boolean String Date RegExp'.split(' '), function(type){
+    zita['is' + type] = function(obj){ return _type(obj) == type.toLowerCase(); };
+});
+
+zita.isUndefined = function(obj){
+    return obj == undefined;
+};
+
+zita.isNull = function(obj){
+    return obj == null;
+};
+
+zita.isArray = Array.isArray || function(obj){
+    return _type(obj) == 'array';
+};
+
+// fix chrome(1-12) bug and use a faster solution
+zita.isFunction = typeof /./ != 'function'
+? function(obj){
+    return typeof obj == 'function';
+}
+: function(obj){
+    return _type(obj) == 'function';
+};
+
+zita.isElement = function(obj){
+    return obj != null && obj.nodeType && obj.nodeType == 1;
+};
+
+zita.isNaN = Number.isNaN || function(obj){
+    return _type(obj) == 'number' && obj !== obj ;
+};
+
+zita.isFinite = function(obj){
+    // !isNaN(parseFloat(obj)) for numeric check
+    // isFinite(obj) for finite check
+    return !isNaN(parseFloat(obj)) && isFinite(obj);
 };
 
 
